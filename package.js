b1d8b0d5f515bb5302279ebf2ceef61f40784833
Luis <luisinweb@gmail.com>
2015-10-08 16:18:11 -0300

    atualizada a versão do pacote

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dinf:loading',
-  version: '1.0.0',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'pacote contendo elementos para apresentação de spin de carregamento',
   // URL to the Git repository containing the source code for this package.
